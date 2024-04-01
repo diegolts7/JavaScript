65f4a973b5574c30b3b88db5d11a367d520cfe57
global <diego7lts7@gmail.com>
2024-04-01 14:33:36 -0300

    add nova questao

--- quest12.js
+++ quest12.js
@@ -1,2 +1,10 @@
-//Escreva uma função chamada verificarPropriedade que aceita um objeto e uma string como argumentos 
-//e verifica se a propriedade especificada existe no objeto. Retorne verdadeiro se existir e falso caso contrário.
+//Escreva uma função chamada verificarPropriedade que aceita um objeto e uma string como argumentos e verifica se a propriedade especificada existe no objeto. Retorne verdadeiro se existir e falso caso contrário.
+let objeto = {
+  nome: '',
+  idade: 13,
+  cpf: 1234
+};
+function verificarPropriedade (string){
+  return string in objeto;
+}
+console.log(verificarPropriedade('cpf'));
